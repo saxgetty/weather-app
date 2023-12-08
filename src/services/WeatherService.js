8a95819a91b3201519fef76582405927f19938ce
Sami <saxgetty4@gmail.com>
2023-12-07 22:02:47 -0800

    Update API urls

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,11 +1,12 @@
 import axios from "axios"
 
-const baseURL = import.meta.env.VITE_OPENWEATHERMAP_BASE_URL
+const dailyWeather = import.meta.env.VITE_OPENWEATHERMAP_DAILY_WEATHER
+const eightDayForecast = import.meta.env.VITE_OPENWEATHERMAP_EIGHT_DAY_FORECAST
 const apiKey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY
 
 export async function fetchWeather(city) {
   try {
-    const response = await axios.get(baseURL, {
+    const response = await axios.get(dailyWeather, {
       params: {
         q: city,
         appid: apiKey,
@@ -19,9 +20,26 @@ export async function fetchWeather(city) {
   }
 }
 
+export async function fetchWeatherForecast(city, numberOfDays) {
+  try {
+    const response = await axios.get(`${eightDayForecast}`, {
+      params: {
+        q: city,
+        appid: apiKey,
+        units: "imperial",
+        cnt: parseInt(numberOfDays),
+      },
+    })
+
+    return response.data
+  } catch (error) {
+    throw new Error(error.response.data.message)
+  }
+}
+
 export async function fetchWeatherByCoords(lat, lon) {
   try {
-    const response = await axios.get(baseURL, {
+    const response = await axios.get(dailyWeather, {
       params: {
         lat,
         lon,
